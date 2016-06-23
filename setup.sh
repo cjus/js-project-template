709e55d567c67b6e15462636d218d1bfa4865efa
Carlos Justiniano <carlos.justiniano@gmail.com>
2016-06-23 13:14:09 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ projectName=${PWD##*/}
 curl -u "$githubUser:$githubToken" https://api.github.com/user/repos -d '{"name":"'$projectName'"}'
 rm -rf .git
 rm setup.sh
+rm README.md
 echo "# $projectName" >> README.md
 git init
 git remote add origin https://github.com/$githubUser/$projectName.git
