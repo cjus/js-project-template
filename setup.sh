a75ed791cf9b7b0e22318c76175398afc02a20e8
cjus <Carlos.Justiniano@gmail.com>
2016-06-23 11:44:22 -0400

    Added support for setting github user name

--- setup.sh
+++ setup.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+githubUser='cjus'
 folderName=${PWD##*/} 
 rm -rf .git
 git init
-git remote add origin https://github.com/cjus/${folderName}.git
+git remote add origin https://github.com/${githubUser}/${folderName}.git
 git add .
 git commit -m "Initial commit"
 git push -u origin master
-open https://github.com/cjus/${folderName}.git
+open https://github.com/${githubUser}/${folderName}.git
 
