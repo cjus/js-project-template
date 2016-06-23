e8ab18f458d242413cafe4d3d0a32c9c7472be14
Carlos Justiniano <carlos.justiniano@gmail.com>
2016-06-23 11:38:52 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,3 +6,5 @@ git remote add origin https://github.com/cjus/${folderName}.git
 git add .
 git commit -m "Initial commit"
 git push -u origin master
+open https://github.com/cjus/${folderName}.git
+
