7f0c08e270930b1aeeada30888fa948d548eae25
cjus <Carlos.Justiniano@gmail.com>
2016-06-23 12:44:27 -0400

    script cleanup

--- setup.sh
+++ setup.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-projectName=$1
+projectName=${PWD##*/}
 curl -u "$githubUser:$githubToken" https://api.github.com/user/repos -d '{"name":"'$projectName'"}'
 rm -rf .git
 git init
-git remote add origin https://github.com/${githubUser}/$projectName.git
+git remote add origin https://github.com/$githubUser/$projectName.git
 git add .
 git commit -m "Initial commit"
 git push -u origin master
-open https://github.com/${githubUser}/$projectName.git
-
+open https://github.com/$githubUser/$projectName.git
